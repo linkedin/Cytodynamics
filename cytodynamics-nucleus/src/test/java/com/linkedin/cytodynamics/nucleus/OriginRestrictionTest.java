37490daeb568fcc41939d881993e93bcca476b63
Cameron Lee <calee@linkedin.com>
2019-09-26 10:06:22 -0700

    Revert "Remove blurb about HTTP redirects" (#6)
    This reverts commit c9df87ff9482e6cbfac971b2355950111c86c1d6.
    
    Reasoning for revert:
    The description of the reverted commit stated that HTTP redirects are
    not followed by URLClassLoader, but it looks like they in fact are
    followed.
    The test that was added by the reverted commit was not properly implemented,
    since the fake HTTP endpoint was not initialized properly. Therefore, the
    fake HTTP redirect never got triggered. The test succeeded because nothing
    got loaded from the fake non-redirect URL, so the class wasn't found. When
    the fake HTTP endpoint was properly initialized using Spark.awaitInitialization,
    then the redirect was triggered, and the class did get loaded.

--- cytodynamics-nucleus/src/test/java/com/linkedin/cytodynamics/nucleus/OriginRestrictionTest.java
+++ cytodynamics-nucleus/src/test/java/com/linkedin/cytodynamics/nucleus/OriginRestrictionTest.java
@@ -7,13 +7,11 @@
  */
 package com.linkedin.cytodynamics.nucleus;
 
-import java.net.URI;
 import java.net.URL;
-import java.util.Collections;
 import org.testng.annotations.Test;
-import spark.Spark;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 
 public class OriginRestrictionTest {
@@ -40,56 +38,5 @@ public class OriginRestrictionTest {
     assertFalse(denyHttpAllowDefault.isAllowed(new URL("http://foo/")));
     assertTrue(denyHttpAllowDefault.isAllowed(new URL("https://foo/")));
     assertTrue(denyHttpAllowDefault.isAllowed(new URL("file:///foo")));
-  }
-
-  @Test
-  public void testFailRedirect() throws Exception {
-    final String ALLOWED_JAR_LOCATION = "http://localhost:4567/test.jar";
-    final String REDIRECTED_JAR_LOCATION = "http://central.maven.org/maven2/log4j/log4j/1.2.17/log4j-1.2.17.jar";
-
-    // Start HTTP server that redirects to another host
-    Spark.get("/test.jar", (request, response) -> {
-      response.redirect(REDIRECTED_JAR_LOCATION);
-      return "";
-    });
-
-    OriginRestriction allowOnlyLocalhost = OriginRestriction
-        .denyByDefault()
-        .allowingGlobPattern(ALLOWED_JAR_LOCATION);
-
-    // Shouldn't be able to load a class through a redirect
-    ClassLoader loader = LoaderBuilder
-        .anIsolatingLoader()
-        .withClasspath(Collections.singletonList(new URI(ALLOWED_JAR_LOCATION)))
-        .withOriginRestriction(allowOnlyLocalhost)
-        .withParentRelationship(DelegateRelationshipBuilder.builder()
-            .withIsolationLevel(IsolationLevel.FULL)
-            .addWhitelistedClassPattern("java.*")
-            .build())
-        .build();
-
-    try {
-      loader.loadClass("org.apache.log4j.Logger");
-      fail("Expected to get a ClassNotFoundException, since it is not allowed to ");
-    } catch (ClassNotFoundException e) {
-      // expected to reach here
-    }
-
-    // Should be able to load the class using the original URL
-    loader = LoaderBuilder
-        .anIsolatingLoader()
-        .withClasspath(Collections.singletonList(new URI(REDIRECTED_JAR_LOCATION)))
-        .withOriginRestriction(OriginRestriction.allowByDefault())
-        .withParentRelationship(DelegateRelationshipBuilder.builder()
-            .withIsolationLevel(IsolationLevel.FULL)
-            .addWhitelistedClassPattern("java.*")
-            .build())
-        .build();
-
-    Class<?> clazz = loader.loadClass("org.apache.log4j.Logger");
-    assertNotNull(clazz);
-
-    // Stop HTTP server
-    Spark.stop();
   }
 }
